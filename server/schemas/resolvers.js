9b6a4f3e2a7b2a739d163bc0a976594d81e558d9
mrbll48 <william.marks87@gmail.com>
2023-08-15 17:29:04 -0700

    updated schema files, auth.js, and server.js. Got graphql playground functional

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,70 @@
 const { User } = require("../models");
+const { GraphQLError } = require("graphql");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return await User.find({});
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id });
+      }
+      throw new GraphQLError("You are not signed in");
+    },
+  },
+
+  Mutation: {
+    login: async (parent, args, context) => {
+      const user = await User.findOne({
+        $or: [{ username: args.username }, { email: args.email }],
+      });
+      if (!user) {
+        throw new GraphQLError("No user with this username/email");
+      }
+
+      const correctPw = await user.isCorrectPassword(args.password);
+
+      if (!correctPw) {
+        throw new GraphQLError("incorrect password");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, args, context) => {
+      const user = await User.create(args);
+
+      if (!user) {
+        throw new GraphQLError("error");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, args, context) => {
+      console.log(context.user._id);
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      } catch (err) {
+        console.log(err);
+        throw new GraphQLError("Error saving book");
+      }
+    },
+
+    removeBook: async (parent, args, context) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: args.bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        throw new GraphQLError("error");
+      }
+      return updatedUser;
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,15 +2,45 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type User {
-    _id: ID!
-    username: String!
-    email: String!
-    password: String!
-    savedBooks: [bookSchema]
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: String
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User!
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: ID
+    image: String
+    link: String
   }
 
   type Query {
-    users: [User]
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(email: String!, password: String!, username: String): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: ID): User
   }
 `;
 
